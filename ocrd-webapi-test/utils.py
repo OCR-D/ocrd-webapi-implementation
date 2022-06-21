02c30d8c95b525d12a6500ba58685f7086a554ed
joschrew <jonas.schrewe@gwdg.de>
2022-06-21 09:18:53 +0200

    some util functions
    functions which I currently not need but I want to keep them for maybe future use

--- ocrd-webapi-test/utils.py
+++ ocrd-webapi-test/utils.py
@@ -1,9 +1,22 @@
 import os
 import asyncio
+import uuid
+import json
 from .constants import (
     SERVER_PATH,
     WORKSPACES_DIR,
 )
+from enum import Enum
+from typing import Dict
+
+__all__ = [
+    "ResponseException",
+    "JobState",
+    "to_workspace_url",
+    "to_workspace_dir",
+    "to_processor_job_dir",
+    "create_processor_job",
+]
 
 
 class ResponseException(Exception):
@@ -13,6 +26,12 @@ class ResponseException(Exception):
     def __init__(self, status_code: int, body: dict | None = None):
         self.status_code = status_code
         self.body = body
+
+
+class JobState(Enum):
+    QUEUED = 1
+    RUNNING = 2
+    STOPPED = 3
 
 
 def to_workspace_url(workspace_id: str) -> str:
@@ -40,3 +59,30 @@ async def validate_workspace(uuid) -> bool:
     await proc.communicate()
 
     return proc.returncode == 0
+
+
+def to_processor_job_dir(job_id) -> str:
+    """
+    returns path to processor-job which is saved as json-txt
+    """
+    return os.path.join(WORKSPACES_DIR, job_id)
+
+
+def create_processor_job(executable, workspace_id) -> Dict:
+    """
+    create a new ProcessorJob and save it to disk
+    """
+    uid = str(uuid.uuid4())
+    document = {
+        "@id": uid,  # TODO: change to url where job can be retrieved (create func like for a ws)
+        "description": "ProcessorJob",
+        "state": JobState.RUNNING.name,
+        "processor": "TODO: put URL here",
+        "workspace": {
+            "@id": to_workspace_url(workspace_id),
+            "description": "Workspace",
+        }
+    }
+    with open(to_processor_job_dir(uid), 'w') as fptr:
+        json.dump(document, fptr)
+    return document
