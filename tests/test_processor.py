546f3b12e5fcfe39375f45710fca60406c9c29f7
joschrew <jonas.schrewe@gwdg.de>
2022-10-24 22:09:07 +0200

    finish test for run processor

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -5,7 +5,11 @@ import os.path
 
 import pytest
 import subprocess
-from ocrd_webapi import constants
+from ocrd_webapi import (
+    constants,
+    utils
+)
+from time import sleep
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -25,9 +29,15 @@ def test_run_prcessor(client, dummy_workspace):
     new_file_grp = "OCR-D-DUMMY"
     params = {"input_file_grps": 'OCR-D-IMG', "output_file_grps": new_file_grp,
               "workspace_id": dummy_workspace, "parameters": {}}
-    resp = client.post(f"/processor/ocrd-dummy", json=params)
+    resp = client.post("/processor/ocrd-dummy", json=params)
+
     # assert
     assert resp.status_code // 100 == 2, f"run-processor returned error-code: {resp.status_code}"
-
-
-
+    job_id = resp.json()['@id'].split("/")[-1]
+    for x in range(0, 10):
+        # try several times because finishing execution needs some time
+        if client.get(f"processor/ocrd-dummy/{job_id}").json()['state'] in ['STOPPED', 'SUCCESS']:
+            break
+        sleep(0.5)
+    path = os.path.join(utils.to_workspace_dir(dummy_workspace), new_file_grp)
+    assert os.path.exists(path), f"ouput_file_grp not created. expecting exists: '{path}'"
