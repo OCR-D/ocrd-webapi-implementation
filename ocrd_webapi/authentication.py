04fc05d163d36bf4e5dff0d9ac9dedf1a1f0772c
mehmedGIT <mehmed.n.mustafa@gmail.com>
2023-05-04 15:40:23 +0200

    remove dummy auth

--- ocrd_webapi/authentication.py
+++ ocrd_webapi/authentication.py
@@ -4,30 +4,6 @@ from typing import Tuple
 
 from .database import create_user, get_user
 
-from os import getenv
-from secrets import compare_digest
-from fastapi import HTTPException, status
-from fastapi.security import HTTPBasicCredentials
-
-
-def dummy_security_check(auth: HTTPBasicCredentials):
-    """
-    Reference security check implementation
-    """
-    user = auth.username.encode("utf8")
-    pw = auth.password.encode("utf8")
-    expected_user = getenv("OCRD_WEBAPI_USERNAME", "test").encode("utf8")
-    expected_pw = getenv("OCRD_WEBAPI_PASSWORD", "test").encode("utf8")
-
-    user_matched = compare_digest(user, expected_user)
-    pw_matched = compare_digest(pw, expected_pw)
-
-    if not user or not pw or not user_matched or not pw_matched:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            headers={"WWW-Authenticate": "Basic"}
-        )
-
 
 async def authenticate_user(email: str, password: str):
     db_user = await get_user(email=email)
